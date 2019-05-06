19c42ca2926c39a4a52cd60cadc0fe7b391305fc
nkononov <Mikalai.Konanau@itechart-group.com>
2019-05-06 12:36:29 +0300

    Small initial refactor

---
+++ src/components/App/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Header</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
