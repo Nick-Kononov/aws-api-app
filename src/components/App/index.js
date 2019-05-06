767d29bd3a91136ee9ed08b5e62e8a239433f6b7
nkononov <Mikalai.Konanau@itechart-group.com>
2019-05-06 14:51:12 +0300

    Update App with AppForm and AppCheckpointList

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap'
 import './index.css';
+
+import AppForm from './../AppForm'
+import AppCheckpointList from './../AppCheckpointList'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h2>Header</h2>
+        <h1>AWS API APP</h1>
       </header>
+      <Container>
+        <Row>
+          <Col className='mr-5'>
+            <AppForm/>
+          </Col>
+          <Col className='ml-5'>
+            <AppCheckpointList/>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
